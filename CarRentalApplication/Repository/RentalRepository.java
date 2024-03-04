9e5467af11c2fe724a674c595a8b84f8d94f8c08
Rubashree A <86885414+ShreeRuba@users.noreply.github.com>
2024-03-04 09:28:31 +0530

    Delete CarRentalApplication directory

--- CarRentalApplication/Repository/RentalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.CarRentalApplication;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RentalRepository extends JpaRepository<Rental, Long> {
-    // Custom queries if needed
-}
